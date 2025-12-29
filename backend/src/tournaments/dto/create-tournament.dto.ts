ccd457d45d4560be3e1bda1ed0e1963dbd2c7127
Pedro Henrique <pedrohal2503@gmail.com>
2025-12-28 22:20:55 -0300

    criando entidade tournament

--- backend/src/tournaments/dto/create-tournament.dto.ts
+++ backend/src/tournaments/dto/create-tournament.dto.ts
@@ -1,1 +1,40 @@
-export class CreateTournamentDto {}
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+} from 'class-validator';
+
+export class CreateTournamentDto {
+  @IsString({ message: 'Nome do torneio deve ser um texto' })
+  @IsNotEmpty({ message: 'Nome do torneio é obrigatório' })
+  @MaxLength(100)
+  tournamentName: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(100)
+  format?: string; // 'suiço' | 'mata-mata'
+
+  @IsDateString(
+    {},
+    { message: 'Data do torneio deve estar no formato AAAA-MM-DD' },
+  )
+  @IsNotEmpty({ message: 'Data do torneio é obrigatória' })
+  tournamentDate: Date;
+
+  @IsNumber(
+    { maxDecimalPlaces: 2 },
+    { message: 'A taxa de entrada deve ser um valor numérico' },
+  )
+  @Min(0, { message: 'Taxa de Inscrição não pode ser negativa' })
+  entryFee: number;
+
+  @IsNumber()
+  @Min(2, { message: 'No mínimo 2 jogadores por torneio' })
+  @IsOptional()
+  maxPlayers?: number;
+}
